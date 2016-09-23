b71914aca6f74e936796119f229c9499dda59058
we5 <bjoern@we5.de>
2016-09-23 10:51:53 +0200

    Use state instead of lastActivity

--- index.js
+++ index.js
@@ -1571,8 +1571,8 @@ FHEMAccessory(platform, s) {
 
   } else if( s.Internals.TYPE == 'RESIDENTS' ) {
     this.service_name = 'security';
-    this.mappings.SecuritySystemCurrentState = { reading: 'state', values: ['/^home/:DISARMED', '/^gotosleep/:NIGHT_ARM', '/^absent/:STAY_ARM', '/^gone/:AWAY_ARM'] }
-    this.mappings.SecuritySystemTargetState = { reading: 'state', values: ['/^home/:DISARMED', '/^gotosleep/:NIGHT_ARM', '/^absent/:STAY_ARM', '/^gone/:AWAY_ARM'], cmds: ['STAY_ARM:home', 'AWAY_ARM:absent', 'NIGHT_ARM:gotosleep', 'DISARM:home'], delay: true }
+    this.mappings.SecuritySystemCurrentState = { reading: 'state', values: ['home:STAY_ARM', 'gotosleep:NIGHT_ARM', 'absent:AWAY_ARM', 'gone:AWAY_ARM', 'off:DISARM ED'] }
+    this.mappings.SecuritySystemTargetState = { reading: 'state', cmd: 'state', values: ['home:STAY_ARM', 'gotosleep:NIGHT_ARM', 'absent:AWAY_ARM', 'gone:AWAY_ARM', 'off:DISARM'], cmds: ['STAY_ARM:home', 'AWAY_ARM:absent', 'NIGHT_ARM:gotosleep', 'DISARM:home'] }
 
   } else if( s.Attributes.model == 'fs20di' )
     this.service_name = 'light';
